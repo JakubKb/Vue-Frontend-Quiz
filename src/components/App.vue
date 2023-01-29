6f840ba74fcbcea7eda8f09926b187c9e5109574
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-01-29 17:47:28 +0100

    Added more questions, and you can choose how much you want.

--- src/components/App.vue
+++ src/components/App.vue
@@ -46,12 +46,14 @@
     </div>
     <div class="preQuiz" id="preQuiz" :style="{ display: displayPreQ }">
       <h2>Frontend Quiz!</h2>
-      <p>In this quiz you'll be answering 3 frontend questions!</p>
+      <p>In this quiz you'll be answering frontend questions!</p>
       <br />
-      <p>This is the 3 question beta! still in development</p>
+      <p>This is a beta version. still in development</p>
       <br />
       <a href="https://github.com/JustKooba/Vue-Frontend-Quiz">Github Repo</a>
-      <button class="startQ" @click="startQuiz">Start Quiz!</button>
+      <button class="startQ" @click="startQuiz">3</button>
+      <button class="startQ" @click="startQuiz">5</button>
+      <button class="startQ" @click="startQuiz">10</button>
     </div>
   </main>
 </template>
@@ -94,13 +96,89 @@ const questions = [
     ],
     selected: null,
   },
+  {
+    id: 3,
+    question: "What is the purpose of the BEM methodology?",
+    answer: "3",
+    options: [
+      "To create responsive designs",
+      "To improve website's SEO",
+      "To simplify the debugging process",
+      "To improve the maintainability and scalability of the code",
+    ],
+    selected: null,
+  },
+  {
+    id: 4,
+    question: "What is the difference between webpack and gulp?",
+    answer: "0",
+    options: [
+      "Webpack is a module bundler, while gulp is a task runner",
+      "Webpack is for frontend development, while gulp is for backend development",
+      "Webpack is for React, while gulp is for Angular",
+      "Webpack is a new technology, while gulp is an old one",
+    ],
+    selected: null,
+  },
+  {
+    id: 5,
+    question: "What is the difference between state and props?",
+    answer: "1",
+    options: [
+      "State is for components, while props is for elements",
+      "State is for internal component's data, while props is for data passed from the parent component",
+      "State is for handling events, while props is for handling data",
+      "State is for handling data, while props is for handling events",
+    ],
+    selected: null,
+  },
+  {
+    id: 6,
+    question: "What is the difference between Redux and MobX?",
+    answer: "2",
+    options: [
+      "Redux is for React, while MobX is for Angular",
+      "Redux is for state management, while MobX is for event management",
+      "Redux uses a centralized store, while MobX uses a decentralized store",
+      "Redux is for managing global state, while MobX is for managing local state",
+    ],
+    selected: null,
+  },
+  {
+    id: 7,
+    question: "What are the benefits of using a CSS preprocessor?",
+    answer: "0",
+    options: [
+      "It allows for the use of variables, functions, and nesting, which can improve code maintainability and reusability",
+      "It improves website's performance",
+      "It allows for the use of JavaScript in CSS",
+      "It makes the design process faster",
+    ],
+    selected: null,
+  },
+  {
+    id: 8,
+    question: "What is the difference between a closure and a scope?",
+    answer: "1",
+    options: [
+      "Closure is a function, while scope is a variable",
+      "Closure is a function that has access to variables in its lexical scope, while scope refers to the accessibility or visibility of variables and functions in certain parts of the code",
+      "Closure is a way to create private variables, while scope is a way to create public variables",
+      "Closure is only used in JavaScript, while scope is used in all programming languages",
+    ],
+    selected: null,
+  },
 ];
+questions.forEach((question) => {
+  question.options.sort(() => Math.random() - 0.5);
+});
 export default {
   data() {
     return {
       questions: questions,
       score: 0,
       currentQuestionIndex: 0,
+      maxQuestionIndex: 0,
       displayContainer: "none",
       displayPreQ: "flex",
       displayCompleted: "none",
@@ -115,6 +193,7 @@ export default {
       buttons.forEach((button) => {
         button.classList.remove("incorrect");
       });
+
       if (index == this.questions[this.currentQuestionIndex].answer) {
         this.score++;
         this.$refs["button" + index][0].classList.add("correct");
@@ -135,7 +214,7 @@ export default {
           this.buttonsDisabled = false;
         }, 3000);
       }
-      if (this.currentQuestionIndex === 2) {
+      if (this.currentQuestionIndex === this.maxQuestionIndex) {
         setTimeout(() => {
           this.$nextTick(() => {
             this.displayContainer = "none";
@@ -151,9 +230,21 @@ export default {
       this.displayContainer = "flex";
       this.displayCompleted = "none";
     },
-    startQuiz() {
-      this.displayContainer = "flex";
-      this.displayPreQ = "none";
+    startQuiz(event) {
+      let buttonValue = event.target.innerText;
+      if (buttonValue === "3") {
+        this.maxQuestionIndex = 2;
+        this.displayContainer = "flex";
+        this.displayPreQ = "none";
+      } else if (buttonValue === "5") {
+        this.maxQuestionIndex = 4;
+        this.displayContainer = "flex";
+        this.displayPreQ = "none";
+      } else if (buttonValue === "10") {
+        this.maxQuestionIndex = 9;
+        this.displayContainer = "flex";
+        this.displayPreQ = "none";
+      }
     },
   },
 };
@@ -186,9 +277,13 @@ body {
 }
 .preQuiz {
   display: flex;
+  text-align: center;
 }
 .preQuiz a {
   margin-bottom: 10px;
+  text-align: center;
+  margin-right: 7px;
+  color: rgb(10, 145, 255);
 }
 .container {
   display: none;
@@ -196,6 +291,12 @@ body {
 #completed button {
   margin-top: 10px;
 }
+
+.startQ {
+  margin-bottom: 10px;
+  width: 100px;
+}
+
 #completed {
   display: none;
   justify-content: center;
